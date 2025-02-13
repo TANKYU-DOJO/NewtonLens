1a47c33310ca7ae022b482dd6aedc2e88410b129
777shuang <shuang777@outlook.jp>
2025-02-13 21:29:48 +0900

    余分な処理を削除

--- Assets/Scripts/TakePicture.cs
+++ Assets/Scripts/TakePicture.cs
@@ -4,35 +4,38 @@ using UnityEngine.UI;
 
 public class TakePicture
 {
-    public RawImage imageDisplay; // 撮影した画像を表示するUI
-    public UnityEvent<string> OnPhotoCaptured; // 画像が撮影されたときに呼び出すイベント
+    //public RawImage imageDisplay; // 撮影した画像を表示するUI
+    //public UnityEvent<string> OnPhotoCaptured; // 画像が撮影されたときに呼び出すイベント
 
     // カメラを起動して写真を撮影するメソッド
-    public void CapturePhoto()
+    public string CapturePhoto()
     {
-        NativeCamera.Permission permission = NativeCamera.TakePicture((path) =>
+        string path = null;
+        NativeCamera.Permission permission = NativeCamera.TakePicture((path_) =>
         {
-            if (path != null)
+            /*if (path_ != null)
             {
                 // 撮影された画像を読み込んでテクスチャとして表示
-                Texture2D texture = NativeCamera.LoadImageAtPath(path);
+                Texture2D texture = NativeCamera.LoadImageAtPath(path_);
                 if (texture == null)
                 {
                     Debug.Log("撮影された画像の読み込みに失敗しました。");
                     return;
                 }
 
-                OnPhotoCaptured.Invoke(path);
+                OnPhotoCaptured.Invoke(p);
 
                 // UIに画像を表示
                 imageDisplay.texture = texture;
-                Debug.Log($"画像パス: ${path}");
-            }
+            }*/
+            path = path_;
         });
 
         if (permission == NativeCamera.Permission.Denied || permission == NativeCamera.Permission.ShouldAsk)
         {
             Debug.Log("カメラの使用許可が必要です。");
         }
+
+        return path;
     }
 }
