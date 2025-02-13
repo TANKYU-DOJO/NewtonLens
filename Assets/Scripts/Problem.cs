8e7621db3d20d5e395491d497cd163a79eacb348
777shuang <shuang777@outlook.jp>
2025-02-13 23:16:03 +0900

    問題の構造を変更

--- Assets/Scripts/Problem.cs
+++ Assets/Scripts/Problem.cs
@@ -8,14 +8,17 @@ namespace ProblemInterpreter
         [System.Serializable]
         public class RigidbodyDefinition {
             public string name; // オブジェクト名
+            public string shape; // 形状。"直方体"または"球体"
             public string mass; // 質量
             public int x, y; // 初期位置
+            public string vx, vy; // 初速度
         }
 
         [System.Serializable]
         public class SpringDefinition {
             public List<string> connections; // ばねが繋いでいる物体の名前
-            public string konstant; // ばね定数
+            //public string konstant; // ばね定数
+            public string length; // 自然長(0~10)
         }
 
         [System.Serializable]
@@ -24,7 +27,7 @@ namespace ProblemInterpreter
             public string length; // 糸の長さ
         }
 
-        public List<string> objects; // "壁"または"床"または"天井"
+        public List<string> environments; // "壁"または"床"または"天井"
         public List<RigidbodyDefinition> rigidbodies; // 剛体
         public List<SpringDefinition> springs; // ばね
         public List<StringDefinition> strings; // 糸
